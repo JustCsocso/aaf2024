52e2a2b42e1b0b222c120feee1bbee54b559f1b0
Pintér Csongor <pinterc21@ady-nagyatad.sulinet.hu>
2024-09-06 13:31:16 +0200

    dw

---
+++ vers/vers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> kuty=new Dictionary<string,int>();
+
+            StreamReader olvas = new StreamReader("koponyeg.txt");
+
+            while (!olvas.EndOfStream)
+            {
+                Console.WriteLine(olvas.ReadToEnd());
+            }
+            olvas.Close();
+        }
+    }
+}
