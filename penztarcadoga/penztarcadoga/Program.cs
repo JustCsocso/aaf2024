04d82661488e58b4ba063e984d757c1be347874e
Pintér Csongor <pinterc21@ady-nagyatad.sulinet.hu>
2024-12-05 08:45:25 +0100

    dw

---
+++ penztarcadoga/penztarcadoga/Program.cs
@@ -0,0 +1,7 @@
+﻿using penztarcadoga;
+
+penztarca apu = new penztarca(2000);
+penztarca fia=new penztarca(2);
+penztarca maria = new penztarca(0);
+Console.WriteLine(apu-fia);
+Console.WriteLine(apu+maria);
