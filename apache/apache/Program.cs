b5685ce26f6e416ed61014fe98ec3ce7ee36ceb6
Pintér Csongor <pinterc21@ady-nagyatad.sulinet.hu>
2024-12-11 13:28:07 +0100

    dw

---
+++ apache/apache/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+string text = File.ReadAllText("adatok.txt");
+
+
+
+Regex minta = new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+
+var illik = minta.Matches(text);
+Console.WriteLine(illik.Count);
