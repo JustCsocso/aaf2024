c8558bcc06b21b245b2ea75ac7b3ed89c5265b36
Pintér Csongor <pinterc21@ady-nagyatad.sulinet.hu>
2025-01-13 14:19:34 +0100

    +

---
+++ gykaszi1113/gykaszi1113/Pont.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gykaszi1113
+{
+    internal class Pont
+    {
+        public int x;
+        public int y;
+        public Pont(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Pont(int x)
+        {
+            this.x = x;
+            y = 0;
+        }
+        public Pont()
+        {
+            y = 0;
+            x = 0;
+        }
+        public static Pont operator +(Pont a, Pont b)
+        {
+            return new Pont(a.x + b.x, a.y + b.y);
+        }
+       
+
+        public static Pont operator *(Pont a, int b)
+        {
+            return new Pont(a.x * b.x, a.y * b.x);
+        }
+
+        public override string ToString()
+        {
+            return "x: " + x + ", y: " + y;
+        }
+    }
+}
---
+++ gykaszi1113/gykaszi1113/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using gykaszi1113;
+
+Pont p1 = new Pont(1, 1);
+Pont p2 = new Pont(2, 3);
+Pont p3 = new Pont(4, 5);
+Pont p4 = new Pont(6, 7);
+Console.WriteLine(p1 + p2);
+Console.WriteLine(p3 + p4);
