1daf8b1c08f6fc23ab04d0a693a3e51905568cc5
Pintér Csongor <pinterc21@ady-nagyatad.sulinet.hu>
2025-04-02 13:14:46 +0200

    dwd

--- SzinuszGorbe/SzinuszGorbe/MainWindow.xaml.cs
+++ SzinuszGorbe/SzinuszGorbe/MainWindow.xaml.cs
@@ -34,29 +34,125 @@ namespace SzinuszGorbe
 
         private void Canvas_Loaded(object sender, RoutedEventArgs e)
         {
+            origoY = Convert.ToInt32(Height / 2);
+            origoX = Convert.ToInt32(r * 1.1);
+
             koordinataRajzol();
+            for(int i = 0; i <= 60; i += 45)
+            {
+                kor(0);
+                sugar(0);
+            }
+            pirosVonal(50);
+            pont(50);
+
         }
         int r = 100;
+        int origoY = 0;
+        int origoX = 0;
 
         void koordinataRajzol()
         {
+             
+
             Line xTengely= new Line();
             xTengely.Stroke = Brushes.Black;
             xTengely.X1 = 0;
-            xTengely.Y1 = Height/2;
+            xTengely.Y1 = origoY;
             xTengely.X2=Width;
-            xTengely.Y2=Height/2;
+            xTengely.Y2=origoY;
 
             canvas.Children.Add(xTengely);
             
             Line yTengely = new Line();
             yTengely.Stroke = Brushes.Black;
-            yTengely.X1 = r*1.1;
+            yTengely.X1 =origoX;
             yTengely.Y1 = 0;
             yTengely.X2 = r * 1.1;
             yTengely.Y2 = Height;
 
             canvas.Children.Add(yTengely);
+
+            for (int i = 0; i < 5; i++) 
+            {
+                Line vonalka= new Line();
+                vonalka.Stroke = Brushes.Black;
+                vonalka.X1 = i * 90 + yTengely.X1;
+                vonalka.Y1=origoY-5;
+                vonalka.X2 = i * 90 + yTengely.X1;
+                vonalka.Y2 = origoY+5;
+
+                canvas.Children.Add(vonalka);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Line vonalka = new Line();
+                vonalka.Stroke = Brushes.Black;
+                vonalka.X1 = yTengely.X1-5;
+                vonalka.Y1 = i*90+(xTengely.Y1-180);
+                vonalka.X2 = yTengely.X1+5;
+                vonalka.Y2 = i * 90+(xTengely.Y2-180);
+
+                canvas.Children.Add(vonalka);
+            }
+           
+        }
+
+        int korX = 0;
+        int korY = 0;
+
+        void kor(int x)
+        {
+            Ellipse kor= new Ellipse();
+            kor.Stroke = Brushes.Blue;
+            kor.Width = 2 * r;
+            kor.Height=2*r;
+            kor.Margin = new Thickness(x -r ,origoY -r ,0,0);
+
+            korX = Convert.ToInt32(x - r + origoX);
+            korY = Convert.ToInt32(origoY);
+            
+            canvas.Children.Add(kor);
+        }
+
+        void sugar(int x)
+        {
+            Line sugar = new Line();
+            sugar.Stroke = Brushes.Black;
+            sugar.StrokeThickness = 5;
+            sugar.X1 = korX;
+            sugar.Y1 = korY;
+            sugar.X2 = x + origoX;
+            sugar.Y2 = Math.Sin(x / 180.0 * Math.PI) * r + origoY;
+
+            canvas.Children.Add(sugar);
+        }
+        void pont(int x)
+        {
+            Ellipse pont= new Ellipse();
+            pont.Stroke = Brushes.Black;
+            pont.Height = r * .1;
+            pont.Width = r * .1;
+            pont.Margin = new Thickness(origoX-pont.Width/2+x,origoY-pont.Height/2,0,0);
+            pont.Fill = Brushes.Black;
+
+            canvas.Children.Add(pont);
+        }
+
+        void pirosVonal(int x)
+        {
+            int magassag=(int)(Math.Sin(x/180.0*Math.PI)*r);
+
+            Line vonal=new Line();
+            vonal.Stroke = Brushes.Red;
+            vonal.StrokeThickness = 3;
+            vonal.X1 = x + origoX;
+            vonal.Y1 = origoY;
+            vonal.X2=x + origoX-magassag;
+            vonal.Y2=origoY-magassag*r;
+
+            canvas.Children.Add(vonal);
         }
     }
 }
