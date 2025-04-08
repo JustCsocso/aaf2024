de8ed0f6b93e925da4804b3940d44d3836be28bc
Pintér Csongor <pinterc21@ady-nagyatad.sulinet.hu>
2025-04-08 12:26:05 +0200

    dw

--- SzinuszGorbe/SzinuszGorbe/MainWindow.xaml.cs
+++ SzinuszGorbe/SzinuszGorbe/MainWindow.xaml.cs
@@ -32,35 +32,13 @@ namespace SzinuszGorbe
         {
             InitializeComponent();
         }
-        int x = 0;
-        void rajzol(object sender, EventArgs e)
-        {
-            
-            origoX = r * 1.1;
-            origoY = Height / 2;
-
-            kordinataRajzol();
-            
-
-            kor(x);
-            sugar(x);
-            
-            pirosVonal(x);
-            pont(x);
-            x += 1;
-        }
 
         private void Canvas_Loaded(object sender, RoutedEventArgs e)
         {
-
             DispatcherTimer timer = new DispatcherTimer();
             timer.Tick += rajzol;
-            timer.Interval = TimeSpan.FromMilliseconds(40);
+            timer.Interval = TimeSpan.FromMilliseconds(10);
             timer.Start();
-
-            
-
-
         }
         int r = 100;
         double origoY = 0;
@@ -118,7 +96,7 @@ namespace SzinuszGorbe
             kor.Stroke = Brushes.Blue;
             kor.Height = 2 * r;
             kor.Width = 2 * r;
-            kor.Margin = new Thickness(origoX + x - dX-r, origoY-r, 0, 0);
+            kor.Margin = new Thickness(origoX + x - dX - r, origoY - r, 0, 0);
 
             korX = Convert.ToInt32(x - r + origoX);
             korY = Convert.ToInt32(origoY);
@@ -128,16 +106,14 @@ namespace SzinuszGorbe
         void sugar(int x)
         {
             int dX = (int)(Math.Cos(x / 180.0 * Math.PI) * r);
-            int magassag = (int)(Math.Sin(x / 180.0 * Math.PI) * r);
-
             Line sugar = new Line();
             sugar.Stroke = Brushes.Black;
-            sugar.StrokeThickness = 3;
-            sugar.X1 = origoX+x-dX;
+            sugar.StrokeThickness = 5;
+            sugar.X1 = origoX + x - dX;
             sugar.Y1 = origoY;
             sugar.X2 = x + origoX;
-            //sugar.Y2 = origoY-Math.Sin(x / 180.0 * Math.PI) * r;
-            sugar.Y2= origoY-magassag;
+            sugar.Y2 = origoY - Math.Sin(x / 180.0 * Math.PI) * r;
+
             canvas.Children.Add(sugar);
         }
 
@@ -164,6 +140,130 @@ namespace SzinuszGorbe
             vonal.Y1 = origoY;
             vonal.X2 = x + origoX;
             vonal.Y2 = origoY - magassag;
+
+            canvas.Children.Add(vonal);
+        }
+        int x = 0;
+        bool novekszik = true;
+        void rajzol(object sender, EventArgs e)
+        {
+            canvas.Children.Clear();
+            origoX = r * 1.1;
+            origoY = Height / 2;
+
+
+            kordinataRajzol();
+            kor(x);
+            sugar(x);
+            pirosVonal(x);
+            pont(x);
+            korivNagy(x);
+            korivKicsi(x);
+            hullam(x);
+            if (novekszik)
+            {
+                x++;
+            }
+            else
+            {
+                x--;
+            }
+            if (x >= 360)
+            {
+                novekszik = false;
+            }
+            if (x <= 0)
+            {
+                novekszik = true;
+            }
+        }
+        void korivNagy(int x)
+        {
+            double dX = (Math.Cos(x / 180.0 * Math.PI) * r);
+            double magassag = (Math.Sin(x / 180.0 * Math.PI) * r);
+            double x1 = origoX + x - dX + r;
+            double y1 = origoY;
+            double x2 = x + origoX;
+            double y2 = origoY - magassag;
+
+
+
+
+            Path path = new Path();
+            path.Stroke = Brushes.Green;
+            path.StrokeThickness = 3;
+
+            PathGeometry geometry = new PathGeometry();
+
+            PathFigure figure = new PathFigure();
+            figure.StartPoint = new Point(x1, y1);
+
+            ArcSegment arc = new ArcSegment();
+            arc.Point = new Point(x2, y2);
+            arc.Size = new Size(r, r);
+            arc.IsLargeArc = x >= 180;
+
+            arc.SweepDirection = SweepDirection.Counterclockwise;
+
+            figure.Segments.Add(arc);
+            geometry.Figures.Add(figure);
+            path.Data = geometry;
+
+            canvas.Children.Add(path);
+        }
+        void korivKicsi(int x)
+        {
+            double dX = (Math.Cos(x / 180.0 * Math.PI) * r);
+            double magassag = (Math.Sin(x / 180.0 * Math.PI) * r);
+            double x1 = origoX + x - dX + r / 10;
+            double y1 = origoY;
+
+            double x2 = (x + origoX - dX - (x + origoX) / 10); 
+            double y2 = origoY - (origoY - magassag) / 10;
+
+
+
+
+            Path path = new Path();
+            path.Stroke = Brushes.Gray;
+            path.StrokeThickness = 1;
+
+            PathGeometry geometry = new PathGeometry();
+
+            PathFigure figure = new PathFigure();
+            figure.StartPoint = new Point(x1, y1);
+
+            ArcSegment arc = new ArcSegment();
+            arc.Point = new Point(x2, y2);
+            arc.Size = new Size(r / 10, r / 10);
+            arc.IsLargeArc = x >= 180;
+
+            arc.SweepDirection = SweepDirection.Counterclockwise;
+
+            figure.Segments.Add(arc);
+            geometry.Figures.Add(figure);
+            path.Data = geometry;
+
+            canvas.Children.Add(path);
+        }
+        PointCollection pontok= new PointCollection();
+        void hullam(int x)
+        {
+            Polyline vonal = new Polyline();
+            vonal.Stroke = Brushes.Red;
+            vonal.StrokeThickness = 3;
+            double magassag=Math.Sin(x/180.0*Math.PI)*r;
+
+            if (novekszik)
+            {
+                pontok.Add(new Point(x + origoX, origoY - magassag));
+            }
+            else
+            {
+                pontok.RemoveAt(x);
+            }
+
+            vonal.Points= pontok;
 
             canvas.Children.Add(vonal);
         }
