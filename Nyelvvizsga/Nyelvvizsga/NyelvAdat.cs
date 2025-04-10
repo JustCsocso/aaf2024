c0607e9aa3e93100cc9706c08fee790d3a3d43f7
Pintér Csongor <pinterc21@ady-nagyatad.sulinet.hu>
2025-04-10 08:45:08 +0200

    wd

---
+++ Nyelvvizsga/Nyelvvizsga/NyelvAdat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyelvvizsga
+{
+    internal class NyelvAdat
+    {
+        public string nyelv;
+        public List<evAdat> vizsgak=new List<evAdat>();
+        public NyelvAdat(string sor, int[]evszamok) 
+        {
+            string[]vag=sor.Split(';');
+            nyelv=vag[0];
+            this.nyelv=vag[1];
+        }
+        public NyelvAdat(string nyelv, int[] adatok, int[]evszamok)
+        {
+            this.nyelv = nyelv;
+            for(int i = 0; i < adatok.Length; i++)
+            {
+                this.vizsgak.Add(new evAdat(evszamok[i],adatok[i]));
+            }
+        }
+    }
+}
---
+++ Nyelvvizsga/Nyelvvizsga/evAdat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nyelvvizsga
+{
+    internal class evAdat
+    {
+        public int ev;
+        public int vizsgadb;
+        public evAdat(int ev,int db)
+        {
+            this.ev = ev;
+            this.vizsgadb = db;
+        }
+    }
+}
